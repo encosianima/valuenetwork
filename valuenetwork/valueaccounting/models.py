e371135356ed0c87a2c69ccd04e1d48f9daadf71
Lynn Foster <foster.j.lynn@gmail.com>
2018-08-06 07:09:32 -0500

    added resource trackingIdentifier to search

--- valuenetwork/valueaccounting/models.py
+++ valuenetwork/valueaccounting/models.py
@@ -1620,6 +1620,8 @@ class EconomicAgent(models.Model):
                         answer.append(res)
                     elif string in res.notes.lower():
                         answer.append(res)
+                    elif string in res.identifier.lower():
+                        answer.append(res)
         return list(set(answer))
 
     def owned_currency_resources(self):
