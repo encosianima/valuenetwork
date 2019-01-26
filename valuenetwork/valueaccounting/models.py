7910cae728e605a6e41ab56a129666b5d5287b2b
Lynn Foster <foster.j.lynn@gmail.com>
2019-01-26 09:08:28 -0600

    pushing so Bob can help with the methods for agent_commitments (not working now!)

--- valuenetwork/valueaccounting/models.py
+++ valuenetwork/valueaccounting/models.py
@@ -927,8 +927,16 @@ class EconomicAgent(models.Model):
     def finished_commitments(self):
         return [p for p in self.involved_in_commitments() if p.finished==True]
 
+    #bob do the next 2 methods, thanks !!!!! <3 <3 <3
+    # they should both work for: 
+    #    finished=True,False,None(include both)
+    #    also_search_children, self explanatory and has code in the current search
+    #    sort descending choice (commits.order_by('-due_date')  should work i think)
+    def agent_commitments(self, finished=None, also_search_children=False, sort_desc=False):
+        return None
+
     # primitive search
-    def search_commitments(self, search_string, finished=None, also_search_children=True):
+    def search_commitments(self, search_string, finished=None, also_search_children=False, sort_desc=False):
         if also_search_children:
             agents = self.with_all_sub_agents() #includes self
         else: #not tested
