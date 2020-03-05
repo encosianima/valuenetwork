0f0da67f037c0d339e5a7590923fd27fbee723c3
bumfresh <bumbum@cooperativa.cat>
2020-03-05 17:32:33 +0100

    fix payment_options translated string

--- work/models.py
+++ work/models.py
@@ -71,26 +71,15 @@ class MembershipRequest(models.Model):
     surname = models.CharField(_('Surname (for individual memberships)'), max_length=255, blank=True)
     requested_username = models.CharField(_('Requested username'), max_length=32)
     email_address = models.EmailField(_('Email address'), max_length=96,)
-    #    help_text=_("this field is optional, but we can't contact you via email without it"))
     phone_number = models.CharField(_('Phone number'), max_length=32, blank=True, null=True)
     address = models.CharField(_('Where do you live?'), max_length=255, blank=True)
     native_language = models.CharField(_('Languages'), max_length=255, blank=True)
     type_of_membership = models.CharField(_('Type of membership'),
         max_length=12, choices=MEMBERSHIP_TYPE_CHOICES,
         default="individual")
-    #membership_for_services = models.BooleanField(_('Membership for services'), default=False,
-    #    help_text=_('you have legal entity and want to offer services or products to the cooperative'))
-    #autonomous_membership = models.BooleanField(_('Autonomous membership'), default=False,
-    #    help_text=_("you don't have legal entity and want to use the cooperative to make invoices either from inside and to outside the cooperative"))
-    #ocp_user_membership = models.BooleanField(_('OCP user membership'), default=False,
-    #    help_text=_('for those that only want to use the OCP platform'))
-    #consumer_membership = models.BooleanField(_('Consumer membership'), default=False,
-    #    help_text=_("you don't offer any product or service but want to consume through it and support the cooperative"))
     number_of_shares = models.IntegerField(_('Number of shares'),
         default=1,
         help_text=_("How many shares would you like to underwrite? Each share is worth 30 Euros"))
-    #work_for_shares = models.BooleanField(_('work for one share'), default=False,
-    #    help_text=_("You can get 1 share for 6 hours of work. If you choose this option, we will send you a list of tasks and the deadline. You won't have full access before the tasks are accomplished."))
     description = models.TextField(_('Description'),
         help_text=_("Describe your project or collective and the skills or abilities you can offer the cooperative"))
     website = models.CharField(_('Website'), max_length=255, blank=True)
@@ -100,7 +89,6 @@ class MembershipRequest(models.Model):
         help_text = _("If you are in the directory at <a href='https://use.fair-coin.org' target='_blank'>use.fair-coin.org</a> please add the URL to your profile."))
     fairmarket = models.CharField(_('FairMarket shop'), max_length=255, blank=True,
         help_text = _("If you have an online shop at <a href='https://market.fair.coop' target='_blank'>market.fair.coop</a> please add the URL to your fair shop."))
-    #how_do_you_know_fc = models.TextField(_('How do you know Freedom Coop?'), blank=True,)
     known_member = models.CharField(_('Are there any FairCoop participant(s) who can give references about you? If so, who?'), max_length=255, blank=True)
     comments_and_questions = models.TextField(_('Comments and questions'), blank=True,)
 
@@ -304,10 +292,11 @@ class Project(models.Model):
                                     ok = u'<span style="color:#090">ok:</span>'
                                     if 'html' in gate:
                                         if not isinstance(gate['html'], unicode) and not isinstance(gate['html'], str):
-                                            html = gate['html'].decode('utf-8')
+                                            html = gate['html']
+                                            html = str(html) #.decode('utf-8')
                                         else:
                                             html = gate['html']
-                                        ok += u' <ul><li>'+html+u'</li></ul>'
+                                        ok += ' <ul><li>'+html+'</li></ul>'
                                     else:
                                         ok = "Error: no html?"
                                 else:
