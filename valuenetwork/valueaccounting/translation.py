ccae4ef21932cb1c8d80961ff5bbc01843f76b8e
bumfresh <bumbum@cooperativa.cat>
2020-03-14 15:52:55 +0100

    translate role name, association names and labels

--- valuenetwork/valueaccounting/translation.py
+++ valuenetwork/valueaccounting/translation.py
@@ -1,7 +1,19 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import EconomicAgent
+from .models import EconomicAgent, AgentResourceRoleType, AgentAssociationType
 
 class EconomicAgentTranslationOptions(TranslationOptions):
     fields = ('name', 'nick', 'url', 'description', 'address', 'email', 'phone_primary', 'photo_url')
 
 translator.register(EconomicAgent, EconomicAgentTranslationOptions)
+
+
+class AgentResourceRoleTypeTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+translator.register(AgentResourceRoleType, AgentResourceRoleTypeTranslationOptions)
+
+
+class AgentAssociationTypeTranslationOptions(TranslationOptions):
+    fields = ('name', 'plural_name', 'label', 'inverse_label')
+
+translator.register(AgentAssociationType, AgentAssociationTypeTranslationOptions)
