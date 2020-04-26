01c3eb0f5403dd7217bdb2761ef605204592c3d6
bumfresh <bumbum@cooperativa.cat>
2020-04-26 19:48:11 +0200

    delete fair TT duplicates

--- work/views.py
+++ work/views.py
@@ -5879,10 +5879,10 @@ def exchange_logging_work(request, context_agent_id, exchange_type_id=None, exch
             if not slot == mem and slot.name == mem.name:
                 print("Duplicate TT (slot), delete? "+str(slot)+" fvs:"+str(slot.facet_values.all()))
                 loger.info("Duplicate TT (slot), delete? "+str(slot)+" fvs:"+str(slot.facet_values.all()))
-                if slot.is_deletable():
+                if slot.is_deletable() and not slot.facet_values.all():
                     print("DELETE empty slot: "+str(slot))
-                    loger.info("DELETE? empty slot: "+str(slot))
-                    #slot.delete()
+                    loger.info("DELETE empty slot: "+str(slot))
+                    slot.delete()
             #tos = slot.agents_to()
             for to in slot.agents_to:
                 if not to in total_agents:
