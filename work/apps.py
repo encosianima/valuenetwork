6010cdb64dc98ff2f2bd41c45ed6ae4f5d427529
bumfresh <bumbum@cooperativa.cat>
2020-05-26 03:23:53 +0200

    filter script if not migrated

--- work/apps.py
+++ work/apps.py
@@ -7,6 +7,19 @@ from django_comments.signals import comment_was_posted #, comment_will_be_posted
 import logging
 logger = logging.getLogger("ocp")
 
+
+def is_database_synchronized(database):
+
+    from django.db.migrations.executor import MigrationExecutor
+    from django.db import connections, DEFAULT_DB_ALIAS
+
+    connection = connections[database]
+    connection.prepare_database()
+    executor = MigrationExecutor(connection)
+    targets = executor.loader.graph.leaf_nodes()
+    return not executor.migration_plan(targets)
+
+
 class WorkAppConfig(AppConfig):
     name = 'work'
     verbose_name = 'Work'
@@ -17,8 +30,9 @@ class WorkAppConfig(AppConfig):
         from work.models import create_unit_types, create_exchange_skills, fill_empty_languages
         from general.models import create_general_types
         from work.signals import comment_notification
+        from django.db import DEFAULT_DB_ALIAS
 
-        if not settings.TESTING:
+        if not settings.TESTING and is_database_synchronized(DEFAULT_DB_ALIAS):
             pre_migrate.connect(fill_empty_languages, sender=self)
 
         post_migrate.connect(create_general_types, sender=self)
